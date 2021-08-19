2eb186bc1710a670937d9d87d8cada51ce5d3e6e
billy8407 <billy8407@gmail.com>
2021-08-19 22:37:55 +0800

    Seperately print file and stream logs

--- utility/function_wrapper.py
+++ utility/function_wrapper.py
@@ -1,5 +1,7 @@
 from functools import wraps
 import time
+import traceback
+
 from utility.log_handler import logger
 
 test_dict = {
@@ -9,12 +11,18 @@ test_dict = {
 
 
 def log_measure(func):
+    log_dict = {'orignal_func': func.__name__}
+
     @wraps(func)
     def wrap(*args, **kwargs):
         try:
             result = func(*args, **kwargs)
-            logger.warning(test_dict['1'])
+            logger.warning(test_dict['1'], extra=log_dict)
+            
             return result
         except:
-            logger.error(test_dict['2'], exc_info=True)
+            # string traceback
+            log_dict['traceback'] = traceback.format_exc()
+            logger.error(test_dict['2'], extra=log_dict)
+    
     return wrap
--- utility/log_handler.py
+++ utility/log_handler.py
@@ -7,7 +7,8 @@ console_level = logging.INFO
 file_level = logging.ERROR
 
 absPath = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-formatter = logging.Formatter('%(asctime)s\n%(filename)s:%(lineno)s - %(funcName)s() - %(message)s')
+file_formatter = logging.Formatter('%(asctime)s\n%(filename)s:%(lineno)s - %(orignal_func)s()\n%(traceback)s')
+stream_formatter = logging.Formatter('%(message)s')
 
 today = datetime.datetime.now()
 stf_date = today.strftime("%Y_%m_%d")
@@ -21,16 +22,15 @@ if folder_name not in os.listdir(absPath):
 folder_path = os.path.join(absPath, folder_name)
 logFile = os.path.join(folder_path, logFile_name)
 
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 file_handler = logging.FileHandler(logFile, 'a', 'utf-8')
 file_handler.setLevel(level=file_level)
-file_handler.setFormatter(formatter)
+file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
 stream_handler = logging.StreamHandler(sys.stdout)
 stream_handler.setLevel(level=console_level)
-stream_handler.setFormatter(formatter)
+stream_handler.setFormatter(stream_formatter)
 logger.addHandler(stream_handler)
