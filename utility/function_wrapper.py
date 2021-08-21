684b33567f809c24763d51be76965094c44f4e79
billy8407 <billy8407@gmail.com>
2021-08-22 00:22:41 +0800

    Prevent no key error in logger print. Feature: Close main process automatically when exception occurs

--- utility/function_wrapper.py
+++ utility/function_wrapper.py
@@ -1,12 +1,12 @@
 from functools import wraps
+import sys
 import time
 import traceback
 
 from utility.log_handler import logger
 
 test_dict = {
-    '1': 'This is a warning',
-    '2': 'This is an error'
+    'x': 'x is error'
 }
 
 
@@ -17,12 +17,19 @@ def log_measure(func):
     def wrap(*args, **kwargs):
         try:
             result = func(*args, **kwargs)
-            logger.warning(test_dict['1'], extra=log_dict)
+            # logger.warning(test_dict[func.__name__], extra=log_dict)
             
             return result
         except:
             # string traceback
             log_dict['traceback'] = traceback.format_exc()
-            logger.error(test_dict['2'], extra=log_dict)
+
+            # Prevent no key error in logger print 
+            if func.__name__ in test_dict.keys():
+                logger.error(test_dict[func.__name__], extra=log_dict)
+            else:
+                logger.error('', extra=log_dict)
+
+            sys.exit(1)
     
     return wrap
